82689944885fdf079b5b0058472afed9341ec023
Li <xiangl12@cc-login3.campuscluster.illinois.edu>
2025-11-30 08:37:21 -0600

    M20

--- js/generation.js
+++ js/generation.js
@@ -76,9 +76,8 @@ function txt2_carousel_item_template(item) {
                 <div style="width: 100%; aspect-ratio: 1">
                     <!--<video autoplay playsinline loop muted height="100%" src="assets/txt2/videos/${item.video}"></video>-->
                     <video autoplay playsinline loop muted height="100%" src="assets/results/ancientFighter.mp4"></video>
-                </div>
-                <div class="caption" style="margin-top:0; display: flex; flex-direction: row; align-items: center;">
-                    <div class="x-handwriting" style="margin-left: 8px;">
+                </div><div class="caption">
+                    <div class="x-handwriting">
                         ${item.prompt}
                     </div>
                 </div>
